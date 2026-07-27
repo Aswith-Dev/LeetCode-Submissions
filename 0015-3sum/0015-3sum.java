b3ba9db72372c1a23fe02dbf0da9f14ab8a41dd8
Ade Aswith <adeashwith@gmail.com>
2026-07-27 19:22:33 +0530

    Time: 158 ms (15.26%), Space: 136.1 MB (5.77%) - LeetHub

---
+++ 0015-3sum/0015-3sum.java
@@ -0,0 +1,41 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+       List<List<Integer>> llist = new ArrayList<>();
+       Arrays.sort(nums);
+       int i = 0;
+       int j = i+1;
+       int k = nums.length-1;
+
+       while(i<k)
+       {
+            j = i+1;
+            k = nums.length-1;
+            while(j<k)
+            {
+                  if(i!=j && i!=k && j!=k)
+                  {
+                        int sum = nums[i]+nums[j]+nums[k];
+                        if(sum == 0)
+                        {
+                              List<Integer> list = new ArrayList<>(Arrays.asList(nums[i],nums[j],nums[k]));
+                              llist.add(list);
+                              k--;
+                        }
+                        else if(sum<0)
+                        {
+                           j++;
+                        }
+                        else
+                        {
+                              k--;
+                        }
+                  }
+            }
+            i++;
+       }
+        
+         List<List<Integer>> ans = new ArrayList<>(new HashSet<>(llist));
+       
+       return ans;  
+    }
+}
