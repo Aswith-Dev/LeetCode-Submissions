8c6cc625732a5a31df1d9afde2b91fbd573367d6
Ade Aswith <adeashwith@gmail.com>
2026-08-23 18:37:34 +0530

    Time: 58 ms (17.22%), Space: 70.5 MB (42.79%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,46 +1,28 @@
 class Solution {
     public int totalFruit(int[] fruits) {
-       int dfg = 0;
-       int n = fruits.length;
-       int r = 0;
        int l = 0;
+       int r = 0;
+       int n = fruits.length;
        int max = 0;
 
        HashMap<Integer,Integer> map = new HashMap<>();
 
        for(r=0; r<n; r++)
        {
-              if(!map.containsKey(fruits[r]))
-              {
-                  dfg++;
-                  map.put(fruits[r],1);  
-              }  
-              else
-              {
-                  map.put(fruits[r],map.get(fruits[r])+1);
-              } 
-      
+          map.put(fruits[r],map.getOrDefault(fruits[r],0)+1);
 
-              if(dfg <= 2)
-              {
-                  max = Math.max(max,r-l+1);
-              }
-              else // dfg > 2
-              {
-                 while(map.get(fruits[l]) > 1)
-                 {
-                    map.put(fruits[l],map.get(fruits[l])-1);
-                    l++;
-                 }
-                 map.put(fruits[l],map.get(fruits[l])-1);
-                 map.remove(fruits[l]);
-                 l++;
-                 dfg--;
-                 
-                 
-              }
+          while(map.size() > 2)
+          {
+             map.put(fruits[l],map.get(fruits[l])-1);
+             if(map.get(fruits[l]) == 0)
+             {
+                map.remove(fruits[l]);
+             }
+             l++;
+          }  
+           
+          max = Math.max(max, r-l+1);
        }
-          
-     return max;
+       return max;
     }
 }
